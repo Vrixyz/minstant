83fcdc9d94b7d3ed55df1c638d1f55f7a31a0b77
Thierry Berger <contact@thierryberger.com>
2023-11-06 09:39:17 +0100

    bevy 0.11; wip selection

--- crates/client/src/main.rs
+++ crates/client/src/main.rs
@@ -1,13 +1,21 @@
 //! Plays animations from a skinned glTF.
 
+use bevy::math::vec4;
+use bevy::{
+    pbr::CascadeShadowConfigBuilder,
+    prelude::*,
+    utils::{HashMap, Instant},
+};
+use bevy_mod_picking::prelude::*;
 use std::f32::consts::PI;
 use std::time::Duration;
 
-use bevy::{animation::RepeatAnimation, pbr::CascadeShadowConfigBuilder, prelude::*};
-
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins((
+            DefaultPlugins.set(low_latency_window_plugin()),
+            DefaultPickingPlugins.build(), //.disable::<DebugPickingPlugin>(),
+        ))
         .insert_resource(AmbientLight {
             color: Color::WHITE,
             brightness: 1.0,
@@ -17,32 +25,25 @@ fn main() {
             Update,
             (setup_scene_once_loaded, keyboard_animation_control),
         )
+        .add_systems(Update, make_pickable)
         .run();
 }
 
 #[derive(Resource)]
 struct Animations(Vec<Handle<AnimationClip>>);
 
-fn setup(
-    mut commands: Commands,
-    asset_server: Res<AssetServer>,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-) {
+#[derive(Resource)]
+struct Models {
+    pub models: HashMap<&'static str, Handle<Scene>>,
+}
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     // Insert a resource with the current scene information
     commands.insert_resource(Animations(vec![asset_server.load("mouse.glb#Animation0")]));
 
     // Camera
     commands.spawn(Camera3dBundle {
-        transform: Transform::from_xyz(100.0, 100.0, 150.0)
-            .looking_at(Vec3::new(0.0, 20.0, 0.0), Vec3::Y),
-        ..default()
-    });
-
-    // Plane
-    commands.spawn(PbrBundle {
-        mesh: meshes.add(shape::Plane::from_size(500000.0).into()),
-        material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
+        transform: Transform::from_xyz(0.0, 1.0, 4.0).looking_at(Vec3::new(0.0, 0.2, 0.0), Vec3::Y),
         ..default()
     });
 
@@ -61,30 +62,80 @@ fn setup(
         .into(),
         ..default()
     });
+    let models = Models {
+        models: HashMap::from([
+            ("mouse".into(), asset_server.load("mouse.glb#Scene0")),
+            ("mouse".into(), asset_server.load("mouse.glb#Scene0")),
+        ]),
+    };
 
-    // Fox
-    commands.spawn(SceneBundle {
-        scene: asset_server.load("mouse.glb#Scene0"),
-        transform: Transform::from_scale(Vec3::splat(40f32)),
-        ..default()
-    });
-
-    println!("Animation controls:");
-    println!("  - spacebar: play / pause");
-    println!("  - arrow up / down: speed up / slow down animation playback");
-    println!("  - arrow left / right: seek backward / forward");
-    println!("  - digit 1 / 3 / 5: play the animation <digit> times");
-    println!("  - L: loop the animation forever");
-    println!("  - return: change animation");
+    // Mouse
+    commands.spawn((
+        SceneBundle {
+            scene: models.models["mouse"].clone_weak(),
+            transform: Transform::from_scale(Vec3::splat(1f32))
+                .with_translation(Vec3::new(-0.5f32, 0f32, 0f32)),
+            //visibility: Visibility::Hidden,
+            ..default()
+        },
+        Champion,
+        Points(0),
+        On::<Pointer<Click>>::target_component_mut::<Points>(|event, points| {
+            info!("Clicked on entity {:?}", event.target);
+            points.0 += 1;
+        }),
+    ));
+    commands.spawn((
+        SceneBundle {
+            scene: models.models["mouse"].clone_weak(),
+            transform: Transform::from_scale(Vec3::splat(1f32))
+                .with_translation(Vec3::new(0.5f32, 0f32, 0f32)),
+            //visibility: Visibility::Hidden,
+            ..default()
+        },
+        PickableBundle::default(),
+        Champion,
+        Points(0),
+        On::<Pointer<Click>>::target_component_mut::<Points>(|event, points| {
+            info!("Clicked on entity {:?}", event.target);
+            points.0 += 1;
+        }),
+    ));
 }
 
+const HIGHLIGHT_TINT: Highlight<StandardMaterial> = Highlight {
+    hovered: Some(HighlightKind::new_dynamic(|matl| StandardMaterial {
+        base_color: matl.base_color + vec4(-0.5, -0.3, 0.9, 0.8), // hovered is blue
+        ..matl.to_owned()
+    })),
+    pressed: Some(HighlightKind::new_dynamic(|matl| StandardMaterial {
+        base_color: matl.base_color + vec4(-0.4, -0.4, 0.8, 0.8), // pressed is a different blue
+        ..matl.to_owned()
+    })),
+    selected: Some(HighlightKind::new_dynamic(|matl| StandardMaterial {
+        base_color: matl.base_color + vec4(-0.4, 0.8, -0.4, 0.0), // selected is green
+        ..matl.to_owned()
+    })),
+};
+fn make_pickable(
+    mut commands: Commands,
+    meshes: Query<Entity, (With<Handle<Mesh>>, Without<Pickable>)>,
+) {
+    for entity in meshes.iter() {
+        dbg!("yo");
+        commands
+            .entity(entity)
+            .insert((PickableBundle::default(), HIGHLIGHT_TINT.clone()));
+    }
+}
 // Once the scene is loaded, start the animation
 fn setup_scene_once_loaded(
     animations: Res<Animations>,
-    mut players: Query<&mut AnimationPlayer, Added<AnimationPlayer>>,
+    mut players: Query<(&mut AnimationPlayer, &mut Visibility), Added<AnimationPlayer>>,
 ) {
     for mut player in &mut players {
-        player.play(animations.0[0].clone_weak()).repeat();
+        //player.0.play(animations.0[0].clone_weak()).repeat();
+        //*player.1 = Visibility::Inherited;
     }
 }
 
@@ -95,34 +146,6 @@ fn keyboard_animation_control(
     mut current_animation: Local<usize>,
 ) {
     for mut player in &mut animation_players {
-        if keyboard_input.just_pressed(KeyCode::Space) {
-            if player.is_paused() {
-                player.resume();
-            } else {
-                player.pause();
-            }
-        }
-
-        if keyboard_input.just_pressed(KeyCode::Up) {
-            let speed = player.speed();
-            player.set_speed(speed * 1.2);
-        }
-
-        if keyboard_input.just_pressed(KeyCode::Down) {
-            let speed = player.speed();
-            player.set_speed(speed * 0.8);
-        }
-
-        if keyboard_input.just_pressed(KeyCode::Left) {
-            let elapsed = player.seek_time();
-            player.seek_to(elapsed - 0.1);
-        }
-
-        if keyboard_input.just_pressed(KeyCode::Right) {
-            let elapsed = player.seek_time();
-            player.seek_to(elapsed + 0.1);
-        }
-
         if keyboard_input.just_pressed(KeyCode::Return) {
             *current_animation = (*current_animation + 1) % animations.0.len();
             player
@@ -132,24 +155,27 @@ fn keyboard_animation_control(
                 )
                 .repeat();
         }
-
-        if keyboard_input.just_pressed(KeyCode::Key1) {
-            player.set_repeat(RepeatAnimation::Count(1));
-            player.replay();
-        }
-
-        if keyboard_input.just_pressed(KeyCode::Key3) {
-            player.set_repeat(RepeatAnimation::Count(3));
-            player.replay();
-        }
-
-        if keyboard_input.just_pressed(KeyCode::Key5) {
-            player.set_repeat(RepeatAnimation::Count(5));
-            player.replay();
-        }
-
-        if keyboard_input.just_pressed(KeyCode::L) {
-            player.set_repeat(RepeatAnimation::Forever);
-        }
     }
+}
+
+fn update_scale_with_points(mut to_scale: Query<(&mut Transform, &Points), Changed<Points>>) {
+    for (mut scale, points) in to_scale.iter_mut() {
+        scale.scale = Vec3::splat(1f32 + points.0 as f32 * 0.1);
+    }
+}
+
+#[derive(Component)]
+pub struct Points(pub i32);
+
+/// Marker, has Points.
+#[derive(Component)]
+pub struct SharedPool;
+
+/// Marker, has Points.
+#[derive(Component)]
+pub struct Champion;
+
+#[derive(Component)]
+pub struct Cooldown {
+    pub ready_at: Instant,
 }
